569fc19d7bcd8dc6781f3a2630d7ad9bc0b0d87c
Andrew Loeliger <loeliger@wisc.edu>
2024-08-22 04:32:14 -0500

    Update excuteCustomPatternTests.py

--- patternGenerator/patternTestingScripts/executeCustomPatternTests.py
+++ patternGenerator/patternTestingScripts/executeCustomPatternTests.py
@@ -7,42 +7,70 @@ import subprocess
 
 console = Console()
 
-def executePatternTest(fileName):
-    #Let's figure out what the name of the file is
-    #Then we generate an output file name based on what that should be
-    #Then, let's actually ssh to krieger and make it run the test
-    filePath = pathlib.Path(fileName)
-    filePlusSuffix = filePath.stem + filePath.suffix
-    fileStem = filePath.stem
-    outputFileName = fileStem+'_output.txt'
+def executePatternTests(fileNames):
+    patternTestScriptLocation = pathlib.Path("patternTestingScripts/executeTest.sh")
+    with open(patternTestScriptLocation, "w") as theFile:
+        theFile.write("cd /cms/aloeliger/anomalyTriggerWork/comissioningCode/CMSSW_14_1_0_pre5/src/CICADADiagnostics/patternGenerator/patternTesting/ && source env.sh\n\n")
+        for fileName in fileNames:
+            filePath = pathlib.Path(fileName)
+            filePlusSuffix = filePath.stem + filePath.suffix
+            fileStem = filePath.stem
+            outputFileName = fileStem+'_output.txt'
+            theFile.write(f'echo \"Running pattern test for file: {fileName}\"\n')
+            theFile.write(f'source pattern_test.sh custom_patterns/{filePlusSuffix} {outputFileName}\n\n')
 
-    ssh_command = f'ssh -t krieger \"cd /cms/aloeliger/anomalyTriggerWork/comissioningCode/CMSSW_14_1_0_pre5/src/CICADADiagnostics/patternGenerator/patternTesting/ && source env.sh && source pattern_test.sh custom_patterns/{filePlusSuffix} {outputFileName}\"'
-    
+    ssh_command = f'ssh -t krieger -t \"source {patternTestScriptLocation.absolute()}\"'
     processResult = subprocess.run(
         [ssh_command],
         shell=True,
-        #check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
+        check=True,
+        stderr = subprocess.PIPE,
     )
 
     if processResult.returncode != 0:
         console.log(f'SSH command to krieger for pattern tests failed.')
-        console.log('Std err of failed attempt:')
+        console.log('stderr of attempt:')
         console.log(processResult.stderr.decode())
+
         exit(1)
+            
+# def executePatternTest(fileName):
+#     #Let's figure out what the name of the file is
+#     #Then we generate an output file name based on what that should be
+#     #Then, let's actually ssh to krieger and make it run the test
+#     filePath = pathlib.Path(fileName)
+#     filePlusSuffix = filePath.stem + filePath.suffix
+#     fileStem = filePath.stem
+#     outputFileName = fileStem+'_output.txt'
+
+#     ssh_command = f'ssh -t krieger \"cd /cms/aloeliger/anomalyTriggerWork/comissioningCode/CMSSW_14_1_0_pre5/src/CICADADiagnostics/patternGenerator/patternTesting/ && source env.sh && source pattern_test.sh custom_patterns/{filePlusSuffix} {outputFileName}\"'
+    
+#     processResult = subprocess.run(
+#         [ssh_command],
+#         shell=True,
+#         #check=True,
+#         stdout=subprocess.PIPE,
+#         stderr=subprocess.PIPE,
+#     )
+
+#     if processResult.returncode != 0:
+#         console.log(f'SSH command to krieger for pattern tests failed.')
+#         console.log('Std err of failed attempt:')
+#         console.log(processResult.stderr.decode())
+#         exit(1)
 
 def main(args):
     #first things first, let's get a list of the files we're going to run pattern tests for
     customPatternDirectory = pathlib.Path('patternTesting/data/custom_patterns')
     patternFiles = list(customPatternDirectory.glob('*.txt'))
-    with console.status('Running pattern tests...'):
-        for fileName in patternFiles:
-            console.log(f'Running pattern test for file: {fileName}')
-            executePatternTest(fileName)
+    #with console.status('Running pattern tests...'):
+    #for fileName in patternFiles:
+    #    console.log(f'Running pattern test for file: {fileName}')
+    #    executePatternTest(fileName)
     #for fileName in track(patternFiles, description='Pattern tests'):
     #    executePatternTest(fileName)
-    console.print(f'Running pattern tests... [bold green]Done![/bold green]')
+    executePatternTests(patternFiles)
+    console.print(f'[bold green]Done![/bold green]')
     
 
 if __name__ == '__main__':
